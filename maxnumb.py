25f3b14902cd1f5b85221ac20027ae68c249556d
Sigrún júnía <sigrunjunia@Sigruns-MacBook-Pro.local>
2020-09-03 14:20:00 +0000

    added a while loop

--- maxnumb.py
+++ maxnumb.py
@@ -5,6 +5,12 @@ num_int = int(input("Input a number: "))    # Do not change this line
 #2. if number form user is not negative print out the largest positive number
 #3. Stop if user inputs negative number
 max_int = 0
+
 while num_int > 0:
+    if num_int > max_int:
+        max_int = num_int
     
+    num_int = int(input("Input a number: "))    # Do not change this line
+
+
 print("The maximum is", max_int)    # Do not change this line
