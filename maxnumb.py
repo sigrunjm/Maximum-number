0d1d16a522ef64306b0339d8f8527e869a5f3738
Sigrún júnía <sigrunjunia@Sigruns-MacBook-Pro.local>
2020-09-03 13:59:37 +0000

    bjó til nýja skrá

---
+++ maxnumb.py
@@ -0,0 +1,10 @@
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+#1. Get input from the user
+#2. if number form user is not negative print out the largest positive number
+#3. Stop if user inputs negative number
+max_int = 0
+while num_int > 0:
+    
+print("The maximum is", max_int)    # Do not change this line
